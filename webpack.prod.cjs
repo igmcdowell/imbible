4bda9a1e1fb8a8a38653f4a82c9fbbb879612a53
Ian McDowell <igmcdowell@gmail.com>
2020-12-14 12:12:31 -0800

    move dist to docs to enable serving for github pages

--- webpack.prod.cjs
+++ webpack.prod.cjs
@@ -6,16 +6,9 @@ const common = require('./webpack.common.cjs');
 
 module.exports = merge.merge(common, {
   mode: 'production',
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.css$/,
-  //     },
-  //   ],
-  // },
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     library: 'imbible',
     libraryTarget: 'umd',
   },
